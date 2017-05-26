bbc3bf143fdd85642c753fc92da51b4eaf80ce95
Mai Hoang <mnhoang.80@gmail.com>
2017-05-26 15:55:16 -0700

    Add path & methodOverride

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-var routes = require('./controllers/burgers_controller.js');
-var port = process.env.PORT || 3000;
-var db = require('./models');
-var app = express();
+var express 		= require('express');
+var bodyParser 		= require('body-parser');
+var exphbs 			= require('express-handlebars');
+var path 			= require("path");
+var methodOverride 	= require("method-override");
+var routes			= require('./controllers/burgers_controller.js');
+var port 			= process.env.PORT || 3000;
+var db 				= require('./models');
+var app 			= express();
 
 app.use(express.static("./public"));
 
@@ -12,6 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(methodOverride("_method"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
